9b187fb726b40958dcc1860ec02b31107638371d
Lennart Cockx <lennart.cockx@gmail.com>
2019-08-13 09:20:43 +0200

    Updated configuration Fixed double mapper error

--- src/main/java/com/toyota/usedcardocker/UsedCarDockerApplication.java
+++ src/main/java/com/toyota/usedcardocker/UsedCarDockerApplication.java
@@ -1,11 +1,9 @@
 package com.toyota.usedcardocker;
 
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDynamoDBRepositories(basePackages = "com.toyota.usedcardocker.repositories")
 public class UsedCarDockerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsedCarDockerApplication.class, args);
--- src/main/java/com/toyota/usedcardocker/configuration/DynamoDBConfig.java
+++ src/main/java/com/toyota/usedcardocker/configuration/DynamoDBConfig.java
@@ -6,22 +6,19 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "com.toyota.usedcardocker.repositories")
 public class DynamoDBConfig {
 
-    @Value("#{awsAccessKey}")
+    @Value("${awsAccessKey}")
     private String amazonDynamoDBAccessKey;
-    @Value("#{awsSecretKey}")
+    @Value("${awsSecretKey}")
     private String amazonDynamoDBSecretKey;
 
     @Bean
@@ -41,11 +38,5 @@ public class DynamoDBConfig {
     @Bean
     public DynamoDB dynamoDB(){
         return new DynamoDB(amazonDynamoDB());
-    }
-
-    @Bean
-    @Primary
-    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB){
-        return new DynamoDBMapper(amazonDynamoDB, DynamoDBMapperConfig.DEFAULT);
     }
 }
